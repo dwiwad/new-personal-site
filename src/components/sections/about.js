385b420b54dd7e308b77e2c403fdebd2edae3226
Dylan Wiwad <dwiwad@gmail.com>
2026-01-29 14:08:45 -0500

    Update the about me page

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['R|Python|SQL', 'SEM|MLM', 'HTML|CSS', 'Javascript', 'Node.js', 'AWS'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,33 +135,37 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Dylan and I enjoy telling stories with data. I received a PhD in
+              Social Psychology in 2019, where I specialized in psychometrics and the psychology of
+              inequality. The main throughline of all my work is my love for (a) finding ways to
+              measure things we cannot observe, and (b) turning big, messy data into fun, useful,
+              and informative insights.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Since getting my PhD I've spent my time as a Postdoc at Northwestern University, in
+              the{' '}
+              <a
+                href="https://www.kellogg.northwestern.edu/academics-research/dispute-resolution-research-center/faculty-and-post-doctoral-fellows/"
+                target="_blank"
+                rel="noreferrer">
+                Kellogg School of Management
+              </a>
+              , but most recently as a Quantitative Researcher at{' '}
+              <a href="https://slack.com/" target="_blank" rel="noreferrer">
+                Slack
+              </a>
+              . I love learning new statistical or analytic methods, and new ways of building fun
+              stuff on the internet.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I also recently <a href="https://www.hockeydecoded.com">launched Hockey Decoded,</a> a
+              website designed to bring advanced hockey analytics out of the depths of stats
+              nerd-dom and into the mainstream.
             </p>
 
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few tools I’ve been working with recently:</p>
           </div>
 
           <ul className="skills-list">
@@ -173,7 +177,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/header.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
